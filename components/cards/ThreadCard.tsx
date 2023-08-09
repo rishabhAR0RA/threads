d1eb67f69424dd80c7b75d92875e5e9503f99b71
Rishabh Arora <rishabharora95.ra@gmail.com>
2023-08-09 21:02:27 +0530

    Fetch thread to Homepage

---
+++ components/cards/ThreadCard.tsx
@@ -0,0 +1,35 @@
+interface Props {
+    id: string;
+    currentUserId: string;
+    parentId: string | null;
+    content: string;
+    author: {
+        name: string;
+        image: string;
+        id: string;
+    }
+    community: {
+        id: string;
+        name: string;
+        image: string;
+    } | null;
+    createdAt: string;
+    comments: {
+        author: {
+            image: string;
+        }
+    }[]
+    isComment?: boolean;
+}
+
+const ThreadCard = ({
+    id,
+    currentUserId,
+    parentId,
+    content,
+    community,
+    createdAt,
+    comments
+}: Props) => {
+
+}

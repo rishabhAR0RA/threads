8400de04090eed49a0cb47ac1c2b2a895b0a3fdf
Rishabh Arora <rishabharora95.ra@gmail.com>
2023-08-12 14:45:20 +0530

    before node module cache clean

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,52 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import PostThread from "@/components/forms/PostThread";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+// import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { profile } from "console";
+import { profileTabs } from "@/constants";
+import Image from "next/image";
+
+async function Page({ params }: { params: { id: string } }) {
+    const user = await currentUser();
+
+    if (!user) return null;
+
+    const userInfo = await fetchUser(params.id);
+
+    if (!userInfo?.onboarded) redirect('/onboarding');
+    return (
+        <section>
+            <ProfileHeader
+                accountId={userInfo.id}
+                authUserId={user.id}
+                name={userInfo.name}
+                username={userInfo.username}
+                imgUrl={userInfo.image}
+                bio={userInfo.bio}
+            />
+
+            <div className="mt-9">
+                {/* <Tabs defaultValue="threads" className="w-full">
+                    <TabsList className="tab">
+                        {profileTabs.map((tab) => (
+                            <TabsTrigger key={tab.label} value={tab.value}>
+                                <Image
+                                    src={tab.icon}
+                                    alt={tab.label}
+                                    width={24}
+                                    height={24}
+                                    className="object-contain"
+                                />
+                                <p className="max-sm:hidden">{tab.label}</p>
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
+                </Tabs> */}
+            </div>
+        </section>
+    )
+}
+
+export default Page;
